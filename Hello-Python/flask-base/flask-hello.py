4bc02dbe4e53bba09fb44f186f84198e3ec4ea0e
AnotherKL <shikieikir@gmail.com>
2018-10-18 23:00:10 +0800

    update

--- Hello-Python/flask-base/flask-hello.py
+++ Hello-Python/flask-base/flask-hello.py
@@ -1,16 +1,31 @@
 # -*- coding: utf-8-*-
 
 from flask import Flask
+from flask import request
+from flask import make_response
+from flask import abort
+from flask.ext.script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello flask!</h1>'
+#    user_agent = request.headers.get('User-Agent')
+#    return '<h1>Hello flask! Your browser is %s</h1>' % user_agent
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
+#    return redirect('https://www.test.com')
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, %s!<h1>' % name
+@app.route('/user/<id>')
+def get_user(id):
+    user = load_user(id)
+    if not user:
+        abort(404)
+    return '<h1>Hello, %s!<h1>' % user.name
 
 if __name__ == '__main__':
-    app.run(debug = True)
+#    app.run(debug = True)
+    manager.run()
+    
